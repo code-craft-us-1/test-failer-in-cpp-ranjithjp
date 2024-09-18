3e38628c74c8c608e4ed9252e7d96a8bf9153fe2
Ranjith J Pillai <ranjithj.pillai@philips.com>
2024-09-18 16:26:58 +0530

    better assert

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,71 @@
 #include <assert.h>
 #include <iostream>
+#include <string>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+
+int BuildColorMap(std::string &colorMap)
+{
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            colorMap.append(std::to_string(i * 5 + j) + " | " + majorColor[i] + " | " + minorColor[j] + "\n");
         }
     }
-    return i * j;
+    return i*j;
 }
 
+int printColorMap() {
+    std::string colorMap;
+    int noOfRows = BuildColorMap(colorMap);
+    std::cout << colorMap;
+    return noOfRows;
+}
+
+//bool TestColorMap(std::string colorMap)
+//{
+//    bool pass = true;
+//    colorMap.substr('\n');
+//}
+
+
 int main() {
+    // Test Number of rows
     int result = printColorMap();
     assert(result == 25);
-    std::cout << "All is well (maybe!)\n";
+
+    // Test expected string.
+    std::string expectedColorMap = "0 | White | Blue\n"
+        "1 | White | Orange\n"
+        "2 | White | Green\n"
+        "3 | White | Brown\n"
+        "4 | White | Slate\n"
+        "5 | Red | Blue\n"
+        "6 | Red | Orange\n"
+        "7 | Red | Green\n"
+        "8 | Red | Brown\n"
+        "9 | Red | Slate\n"
+        "10 | Black | Blue\n"
+        "11 | Black | Orange\n"
+        "12 | Black | Green\n"
+        "13 | Black | Brown\n"
+        "14 | Black | Slate\n"
+        "15 | Yellow | Blue\n"
+        "16 | Yellow | Orange\n"
+        "17 | Yellow | Green\n"
+        "18 | Yellow | Brown\n"
+        "19 | Yellow | Slate\n"
+        "20 | Violet | Blue\n"
+        "21 | Violet | Orange\n"
+        "22 | Violet | Green\n"
+        "23 | Violet | Brown\n"
+        "24 | Violet | Slate\n";
+    std::string colorMap;
+    BuildColorMap(colorMap);
+    assert(expectedColorMap == colorMap);
+    std::cout << "All is well\n";
     return 0;
 }
--- weatherreport.cpp
+++ weatherreport.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <iostream>
 
-using std::cout, std::endl, std::string;
+using std::cout;
+using std::endl;
+using std::string;
 
 namespace WeatherSpace {
 class IWeatherSensor {
@@ -36,6 +38,26 @@ class SensorStub : public IWeatherSensor {
     }
 };
 
+
+class SecondSensorStub : public IWeatherSensor {
+public:
+    int Humidity() const override {
+        return 72;
+    }
+
+    int Precipitation() const override {
+        return 70;
+    }
+
+    double TemperatureInC() const override {
+        return 26;
+    }
+
+    int WindSpeedKMPH() const override {
+        return 30;
+    }
+};
+
 // This is a function to predict the weather, based on readings
 // from a sensor
 
@@ -63,22 +85,21 @@ void TestRainy() {
 }
 
 // Test another rainy day
-
 void TestHighPrecipitationAndLowWindspeed() {
     // This instance of stub needs to be different-
     // to give high precipitation (>60) and low wind-speed (<50)
-    SensorStub sensor;
+    SecondSensorStub sensor;
 
     // strengthen the assert to expose the bug
     // (function returns Sunny day, it should predict rain)
     string report = Report(sensor);
-    assert(report.length() > 0);
+    assert(report.find("rain") != string::npos);
 }
 }  // namespace WeatherSpace
 
 int main() {
     WeatherSpace::TestRainy();
     WeatherSpace::TestHighPrecipitationAndLowWindspeed();
-    cout << "All is well (maybe)\n";
+    cout << "All is well\n";
     return 0;
 }
